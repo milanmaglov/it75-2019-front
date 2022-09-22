66f4969adab279796e6578313583d32924b422cc
milanmaglov <maglov.milan@gmail.com>
2022-09-22 19:45:53 +0200

    Frontend v2.0

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
-export const LIGA_URI = 'https://it75-2019.herokuapp.com/liga';
-export const NACIONALNOST_URI = 'https://it75-2019.herokuapp.com/nacionalnost';
-export const TIM_URI = 'https://it75-2019.herokuapp.com/tim';
-export const IGRAC_URI = 'https://it75-2019.herokuapp.com/igrac';
-export const IGRAC_FOR_URI = 'https://it75-2019.herokuapp.com/igrac/tim'
+export const LIGA_URI = 'https://it75-2019-rva.herokuapp.com/liga';
+export const NACIONALNOST_URI = 'https://it75-2019-rva.herokuapp.com/nacionalnost';
+export const TIM_URI = 'https://it75-2019-rva.herokuapp.com/tim';
+export const IGRAC_URI = 'https://it75-2019-rva.herokuapp.com/igrac';
+export const IGRAC_FOR_URI = 'https://it75-2019-rva.herokuapp.com/igrac/tim'
